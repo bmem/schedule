17cfc5bb0afbe3e7847e63fd9934d736945ac258
Trevor Stone <github@trevorstone.org>
2012-01-01 00:53:04 -0700

    Add date_picker formtastic type

--- app/assets/javascripts/schedule/application.js
+++ app/assets/javascripts/schedule/application.js
@@ -11,5 +11,10 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
+//= require jquery-ui
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function () {
+  $('input.datepicker').datepicker();
+});
