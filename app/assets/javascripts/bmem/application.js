176531d03bc87cd503dbe13a379b2d27ddea9f9c
Trevor Stone <github@trevorstone.org>
2012-01-08 14:49:20 -0700

    Move common JS to bmem assets, only include engine assets in that engine

--- app/assets/javascripts/bmem/application.js
+++ app/assets/javascripts/bmem/application.js
@@ -10,5 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require bmem/application
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
 //= require_tree .
--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -12,5 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require schedule
+//= require bmem/application
 //= require_tree .
