cb4d36f17d86b0460359872e589fec2c39ac8027
Trevor Stone <github@trevorstone.org>
2012-01-08 02:25:23 -0700

    A way to default methods that's less likely to step on helpers

--- lib/bmem/controller.rb
+++ lib/bmem/controller.rb
@@ -1,8 +1,19 @@
 module Bmem
   class Controller < ActionController::Base
+    def method_missing(sym, *args)
+      return super if sym.to_s.start_with? 'default_'
+      with_default = "default_#{sym}".to_sym
+      if self.respond_to?(with_default)
+        send with_default, *args
+      else
+        super
+      end
+    end
+
+    protected
     # The user executing the current request, or nil if no user is logged in.
     # The main app is expected to override this method
-    def current_user
+    def default_current_user
       nil
     end
   end
