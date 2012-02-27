005316417610f754ac8db8eb2ef7cea8ae112ddc
Trevor Stone <github@trevorstone.org>
2012-02-27 00:00:00 -0700

    Flash an alert and redirect instead of an exception error page if cancan denies an action

--- app/controllers/bmem/application_controller.rb
+++ app/controllers/bmem/application_controller.rb
@@ -2,6 +2,11 @@ module Bmem
   class ApplicationController < ActionController::Base
     helper Bmem::TitleHelper
 
+    rescue_from CanCan::AccessDenied do |ex|
+      redirect_back :alert => ex.message
+    end
+
+
     def method_missing(sym, *args)
       return super if sym.to_s.start_with? 'default_'
       with_default = "default_#{sym}".to_sym
@@ -17,6 +22,15 @@ module Bmem
     # The main app is expected to override this method
     def default_current_user
       nil
+    end
+
+    def redirect_back(*args)
+      target = root_url
+      if request.env["HTTP_REFERER"].try {|s| s.start_with? root_url}
+        # user came from our site
+        target = :back
+      end
+      redirect_to target, *args
     end
   end
 end
