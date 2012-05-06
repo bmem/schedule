c6dfedfd03822575d2b0a77091c5977911aadf5c
Trevor Stone <github@trevorstone.org>
2012-05-06 17:24:14 -0600

    Move title logic to ActiveRecord::Base, use it as default to_s

--- app/helpers/bmem/title_helper.rb
+++ app/helpers/bmem/title_helper.rb
@@ -1,7 +1,5 @@
 module Bmem
   module TitleHelper
-    TITLE_ATTRIBUTES = [:title, :display_name, :name, :full_name]
-
     def page_title
       chunks = []
       if instance_variable_defined? '@page_title' then
@@ -23,17 +21,7 @@ module Bmem
     # The title of the record being displayed or edited, may be nil
     def record_title
       if controller.respond_to? :subject_record then
-        controller.subject_record.try do |item|
-          generic = item.class.model_name.human
-          title = if item.new_record? then
-            "New #{generic}"
-          else
-            title_sym = TITLE_ATTRIBUTES.find do |sym|
-              item.respond_to? sym
-            end
-            title_sym ? item.send(title_sym) : "#{generic} #{item.id}"
-          end
-        end
+        controller.subject_record.try { |item| item.to_title }
       end
     end
   end
